587e46dd112e95c88b763438f263cefd72357cc4
Pilar Abarca <mariapilar.abarca@gmail.com>
2022-10-26 20:44:07 -0300

    pokeApi

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,14 @@
-import imagen from './assets/img/pokemon-logo.jpg'
-
-import './assets/css/App.css'
-
-import React, { useEffect, useState } from 'react'
-import CardPokemon from './components/CardPokemon'
-import Search from './components/Search'
-function App() {
-    //setear los hooks useState
-  
-  const [listPokemon, setListPokemon]= useState([]);
-  const [search, setSearch] = useState("")
-  /* Crear referencia sección API */
-  // const apiURL =  `https://pokeapi.co/api/v2/pokemon/${id}`
-  const apiURL =  `https://pokeapi.co/api/v2/pokemon/`
-// console.log(apiURL)
-  // /* Crear Función para obtener personajes */
-  async function getpokemon(apiURL) {
-    const res = await fetch(apiURL)
-    const data = await res.json()
-    console.log(data)
-
-    }
-    useEffect(() => {
-      getpokemon(apiURL)
-
-  }, [])
-
-  // const Pokemon =()=>{
-  //   nombre: data.name;
-  //   img: data.url;
-  // }
+import "./assets/css/App.css";
+import Search from "./components/Search";
+import pokemon from './assets/img/pokemon-logo.jpg'
+const App = () => {
   return (
-    <div className="App">
-     <img src={imagen} alt="" />
-     <Search/>
-     <CardPokemon listPokemon={listPokemon} search={search}/>
+    <div className="app">
+     <img src={pokemon} />
+     <h1>PokeGuia</h1>
+      <Search />
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,22 +1,71 @@
+import { useState } from "react";
+import Axios from "axios";
+
+const Search = () => {
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonChosen, setPokemonChosen] = useState(false);
+  const [pokemon, setPokemon] = useState({
+    name: "",
+    species: "",
+    img: "",
+    hp: "",
+    attack: "",
+    defense: "",
+    type: "",
+  });
+
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
+      (response) => {
+        console.log(response);
+        setPokemon({
+          name: pokemonName,
+          species: response.data.species.name,
+          img: response.data.sprites.front_default,
+          hp: response.data.stats[0].base_stat,
+          attack: response.data.stats[1].base_stat,
+          defense: response.data.stats[2].base_stat,
+          type: response.data.types[0].type.name,
+          ability: response.data.abilities.ability,
 
 
-const Search = ({setSearch}) => {
-  //setear los hooks useState
-  
-  const validarDatos =(e) => {
-    e.preventDefault();
-  }
-    const searcher = (e) => {
-      setSearch(e.target.value)   
-      
-  }
- 
-	return (
-		<form onSubmit={validarDatos} >
-			<input type="text"  onChange={searcher} placeholder="Nombre"       />
-			<button type="submit">BUSCAR</button>
-		</form>
-	)
+	
+
+        });
+        setPokemonChosen(true);
+      }
+    );
+  };
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => {
+            setPokemonName(e.target.value);
+          }}
+        />
+        <button onClick={searchPokemon}>buscar</button>
+      </div>
+      <div>
+        {!pokemonChosen ? (
+          <h1>no hay pokemom</h1>
+        ) : (
+          <>
+           
+            <h1>{pokemon.name}</h1>
+            <img src={pokemon.img} />
+			<h3>species : {pokemon.species}</h3>
+			<h3>Type: {pokemon.type}</h3>
+			<h4>hp: {pokemon.hp}</h4>
+			<h4>attack: {pokemon.attack}</h4>
+			<h4>defense: {pokemon.defense}</h4>
+			<h4>ability: {pokemon.ability}</h4>
+          </>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Search;
